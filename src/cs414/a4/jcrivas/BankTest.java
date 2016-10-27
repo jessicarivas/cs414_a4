b2227b14ed43c59f72a85c9786fb95fc301fdfbf
unknown <jessicarivas92@gmail.com>
2016-10-26 20:04:42 -0600

    add bank tests

---
+++ src/cs414/a4/jcrivas/BankTest.java
@@ -0,0 +1,45 @@
+package cs414.a4.jcrivas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BankTest {
+		
+		
+	private ParkingGarage garage;
+	
+	@Before
+	public void initialize() {
+		garage = new ParkingGarage();
+	}
+
+	@Test
+	public void testBankInitial() {
+		Bank bank = new Bank();
+		assertEquals(0, bank.getTotal());
+	}
+
+	@Test
+	public void testBankAddPayment() {
+		Bank bank = new Bank();
+		bank.addPayment(12);
+		assertEquals(12, bank.getTotal());
+	}
+	
+	@Test
+	public void testBankAddPaymentNegative() {
+		Bank bank = new Bank();
+		bank.addPayment(-12);
+		assertEquals(-12, bank.getTotal());
+	}
+
+	@Test
+	public void testBankAddPaymentMultiple() {
+		Bank bank = new Bank();
+		bank.addPayment(-12);
+		bank.addPayment(36);
+		assertEquals(24, bank.getTotal());
+	}
+}
